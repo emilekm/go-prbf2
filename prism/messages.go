a15416aec228fb25056c601c1f24a468dcaa9449
cassius23 <emilekm@gmail.com>
2024-04-23 16:27:54 +0200

    Move errors

--- prism/messages.go
+++ prism/messages.go
@@ -2,7 +2,6 @@ package prism
 
 import (
 	"bytes"
-	"fmt"
 )
 
 type MessageDecoder interface {
@@ -120,42 +119,4 @@ func (m *KillMessages) Decode(content []byte) error {
 	}
 
 	return nil
-}
-
-type ErrorCode int
-
-const (
-	ErrorCodeUnknown         ErrorCode = iota
-	ErrorCodeUnauthenticated ErrorCode = iota + 3000
-	ErrorCodeIncorectUsernameOrPassword
-	ErrorCodeInssuficientPermissions
-	ErrorCodeAccountExists
-	ErrorCodeOwnAccont
-	ErrorCodeSuperUserLastAccount
-	ErrorCodeDeletedUser
-	ErrorCodeServerVersion
-)
-
-var errorSubjects = []Subject{
-	SubjectCriticalError,
-	SubjectError,
-}
-
-type Error struct {
-	Code    ErrorCode
-	Content string
-}
-
-func (e Error) Subject() Subject {
-	return SubjectError
-}
-
-func (e Error) Error() string {
-	return fmt.Sprintf("ErrorCode: %d, Content: %s", e.Code, e.Content)
-}
-
-type CriticalError Error
-
-func (e CriticalError) Subject() Subject {
-	return SubjectCriticalError
 }
